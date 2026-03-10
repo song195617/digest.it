326aad81f05274af85ed05c871cd43302186b38b
Song Shiyu <song195617@outlook.com>
2026-03-10 20:06:50 +0800

    build: bump android version to 1.3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.digestit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.3.8"
+        versionCode = 13
+        versionName = "1.3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
