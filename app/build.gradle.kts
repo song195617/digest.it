9218f06104c1f84fda7765ec1f72a7fc27f1f605
Song Shiyu <song195617@outlook.com>
2026-03-10 18:40:26 +0800

    build: bump android version to 1.3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.digestit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.3.7"
+        versionCode = 12
+        versionName = "1.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
